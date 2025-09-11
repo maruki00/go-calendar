f420f02b506065c95c0af8c03f2d77e11ff09951
maruki00 <aitbaha48@gmail.com>
2025-09-11 19:01:54 +0100

    Add or update calenderEntity.go

---
+++ internal/event/domain/calenderEntity.go
@@ -0,0 +1,22 @@
+package domain
+
+/**
+{
+  	// title: 'Lunch',
+  	// start: new Date(y, m, d, 12, 0),
+  	// end: new Date(y, m, d, 14, 0),
+  	// allDay: false,
+  	// backgroundColor: '#00c0ef', //Info (aqua)
+  	// borderColor: '#00c0ef' //Info (aqua)
+}
+*/
+
+type Event struct {
+	Id              string `json:"id"`
+	Title           string `json:"title"`
+	StartAt         string `json:"start"`
+	EndAt           string `json:"end"`
+	BackgroundColor string `json:"backgroundColor"`
+	BorderColor     string `json:"borderColor"`
+	AllDay          bool   `json:"allDay"`
+}
